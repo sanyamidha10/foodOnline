451f85ef6196562a60def1d8ecb7324f4285e4db
sanyamidha.10 <sanyamidha.10@gmail.com>
2022-10-14 22:23:07 +0530

    Making of dynamic vendor dashboard

---
+++ vendor/urls.py
@@ -0,0 +1,13 @@
+from django.urls import path, include
+from . import views
+from accounts import views as AccountViews
+
+
+urlpatterns = [
+    path('', AccountViews.vendorDashboard),
+    path('profile/', views.vprofile, name='vprofile'),
+
+]
+
+
+#myAccount will decide which user is this vendor or customer, according to it, this will send user to customerDashboard or vendorDashboard
