8c855d7dc2a936f558b2189f9fc0b64a77c51ea6
sanyamidha.10 <sanyamidha.10@gmail.com>
2022-11-03 13:16:38 +0530

    Edit category

--- vendor/urls.py
+++ vendor/urls.py
@@ -11,7 +11,7 @@ urlpatterns = [
 
     #CATEGORY CRUD
     path('menu-builder/category/add', views.add_category, name='add_category'),
-
+    path('menu-builder/category/edit/<int:pk>/', views.edit_category, name='edit_category'),
 ]
 
 
