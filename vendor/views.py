f1f7fe7addec7d80480b24fa0d26a2bdeba10210
sanyamidha.10 <sanyamidha.10@gmail.com>
2022-11-03 13:38:54 +0530

    Delete category

--- vendor/views.py
+++ vendor/views.py
@@ -109,3 +109,11 @@ def edit_category(request, pk=None):
     }
 
     return render(request, 'vendor/edit_category.html', context)
+
+
+def delete_category(request, pk=None):
+    category = get_object_or_404(Category, pk=pk)
+    category.delete()
+    messages.success(request, 'Category has been deleted successfully!')
+    return redirect('menu_builder')
+    
