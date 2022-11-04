0c3c443f56234ed6610cd3ecb080a0e80f75df35
sanyamidha.10 <sanyamidha.10@gmail.com>
2022-11-04 23:57:05 +0530

    edit food items

--- vendor/urls.py
+++ vendor/urls.py
@@ -16,6 +16,8 @@ urlpatterns = [
 
     #FOODITEMS CRUD
     path('menu-builder/food/add', views.add_food, name='add_food'),
+    path('menu-builder/food/edit/<int:pk>/', views.edit_food, name='edit_food'),
+
 
 ]
 
