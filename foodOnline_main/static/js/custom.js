20a331906a7867c223171072521748b3e878e03d
Sanya Midha <sanyamidha.10@gmail.com>
2023-01-11 13:33:34 +0530

    Send ajax request to add hour function, saving to database and json response

--- foodOnline_main/static/js/custom.js
+++ foodOnline_main/static/js/custom.js
@@ -218,11 +218,54 @@ $(document).ready(function() {
         var from_hour = document.getElementById('id_from_hour').value;
         var to_hour = document.getElementById('id_to_hour').value;
         var is_closed = document.getElementById('id_is_closed').checked;
-        var csr_token = $('input[name=csrfmiddlewaretoken]').val();
+        var csrf_token = $('input[name=csrfmiddlewaretoken]').val();
+        var url = document.getElementById('add_hour_url').value;
 
-        console.log(day, from_hour, to_hour, is_closed, csr_token);
+        console.log(day, from_hour, to_hour, is_closed, csrf_token);
 
+        if(is_closed){
+            is_closed = 'True'
+            condition = "day!=''"
+        }
+        else{
+            is_closed = 'False'
+            condition = "day!='' && from_hour!='' && to_hour!=''"
+        }
 
+        if(eval(condition)){
+            $.ajax({
+                type: 'POST',
+                url: url,
+                data: {
+                    'day': day,
+                    'from_hour': from_hour,
+                    'to_hour' : to_hour,
+                    'is_closed': is_closed,
+                    'csrfmiddlewaretoken': csrf_token,
+                },
+                success: function(response){
+                    if(response.status == 'success'){
+                        if (response.is_closed == 'Closed'){
+                            html = '<tr><td><b>'+response.day+'</b></td><td>Closed</td><td><a href="#">Remove</a></td></tr>'
+                        }
+                        else{
+                            html = '<tr><td><b>'+response.day+'</b></td><td>'+response.from_hour+'-'+response.to_hour+'</td><td><a href="#">Remove</a></td></tr>'   
+                        }
+                    
+                        $(".opening_hours").append(html)
+                        document.getElementById('opening_hours').reset()
+                    }
+                    else{
+                        swal(response.status, '', "error")
+                    }
+
+                }
+            })
+            
+        }
+        else{
+            swal('Please fill all fields', '', 'info');
+        }
     })
 
 });
--- vendor/views.py
+++ vendor/views.py
@@ -1,3 +1,5 @@
+from django.db import IntegrityError
+from django.http import JsonResponse
 from django.shortcuts import get_object_or_404, redirect, render, HttpResponse
 from django.contrib import messages
 from accounts.models import UserProfile
@@ -195,4 +197,25 @@ def opening_hours(request):
     return render(request, 'vendor/opening_hours.html', context)
 
 def add_opening_hours(request):
-    return HttpResponse('Add Opening Hour')
+    if request.user.is_authenticated:
+        if request.headers.get('x-requested-with') == 'XMLHttpRequest' and request.method=='POST':
+            day = request.POST.get('day')
+            from_hour = request.POST.get('from_hour')
+            to_hour = request.POST.get('to_hour')
+            is_closed = request.POST.get('is_closed')
+
+            try:
+                hour = OpeningHour.objects.create(vendor=get_vendor(request), day=day, from_hour=from_hour, to_hour=to_hour, is_closed=is_closed)
+                if hour:
+                    day = OpeningHour.objects.get(id=hour.id)
+                    if(day.is_closed):
+                        response = {'status':'success', 'id':hour.id, 'day':day.get_day_display(), 'is_closed':'Closed'}
+                    else:
+                        response = {'status':'success', 'id':hour.id, 'day':day.get_day_display(), 'from_hour':hour.from_hour, 'to_hour':hour.to_hour}
+                return JsonResponse(response)
+            except IntegrityError as e:
+                response = {'status':'failed'}
+                return JsonResponse(response)
+                
+        else:
+            return HttpResponse('Invalid request')
