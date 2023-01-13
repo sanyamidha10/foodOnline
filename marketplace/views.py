ad94ba2c4a267f9c026c3ef38083fbb082cf5056
Sanya Midha <sanyamidha.10@gmail.com>
2023-01-13 22:28:42 +0530

    Display opening hours on vendor detail page

--- marketplace/views.py
+++ marketplace/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render,redirect
 from marketplace.context_processors import get_cart_counter, get_cart_amounts
 from menu.models import Category, FoodItem
-from vendor.models import Vendor
+from vendor.models import Vendor, OpeningHour
 from django.shortcuts import get_object_or_404
 from django.db.models import Prefetch
 from django.http import HttpResponse, JsonResponse
@@ -12,7 +12,7 @@ from django.db.models import Q
 from django.contrib.gis.geos import GEOSGeometry
 from django.contrib.gis.measure import D # ``D`` is a shortcut for ``Distance``
 from django.contrib.gis.db.models.functions import Distance
-
+from datetime import date
 
 
 # Create your views here.
@@ -34,6 +34,11 @@ def vendor_detail(request, vendor_slug):
         )
     )
 
+    opening_hours = OpeningHour.objects.filter(vendor=vendor).order_by('day', '-from_hour')
+    today_date = date.today()
+    today = today_date.isoweekday()
+    current_opening_hour = OpeningHour.objects.filter(vendor=vendor, day=today)
+
     if request.user.is_authenticated:
         cart_items = Cart.objects.filter(user=request.user)
     else:
@@ -43,6 +48,8 @@ def vendor_detail(request, vendor_slug):
         'vendor':vendor,
         'categories': categories,
         'cart_items': cart_items,
+        'opening_hours': opening_hours,
+        'current_opening_hour': current_opening_hour,
     }
     return render(request, 'marketplace/vendor_detail.html', context)
 
