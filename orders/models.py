b7b89bbc217c14972caacc0e3a768950c9a97911
Sanya Midha <sanyamidha.10@gmail.com>
2023-01-29 20:57:05 +0530

    Calculate vendor order total data and update in vendor order detail page

--- orders/models.py
+++ orders/models.py
@@ -64,13 +64,32 @@ class Order(models.Model):
 
     def get_total_by_vendor(self):
         vendor = Vendor.objects.get(user=request_object.user)
+        subtotal=0
+        tax=0
+        tax_dict={}
         if self.total_data:
             total_data = json.loads(self.total_data)
             data = total_data.get(str(vendor.id))
-            print(data)
-        return vendor
+            
+            for key, val in data.items():
+                subtotal += float(key)
+                val = val.replace("'", '"')
+                val = json.loads(val)
+                tax_dict.update(val)
 
+                # calculate tax
+                # {'CGST': {'9.00': '54.00'}, 'SGST': {'7.00': '42.00'}}
+                for i in val:
+                    for j in val[i]:
+                        tax += float(val[i][j])
 
+        grand_total = float(subtotal)+ float(tax)
+        context={
+            'subtotal': subtotal,
+            'tax_dict': tax_dict,
+            'grand_total': grand_total,
+        }
+        return context
 
     def __str__(self):
         return self.order_number
