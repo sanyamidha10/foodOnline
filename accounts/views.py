c1849d781595616ae1009756d3ed51997962df2e
Sanya Midha <sanyamidha.10@gmail.com>
2023-06-19 17:23:14 +0530

    Message content change on registration

--- accounts/views.py
+++ accounts/views.py
@@ -62,7 +62,7 @@ def registerUser(request):
             email_template = 'accounts/emails/account_verification_email.html'
             send_verification_email(request, user, mail_subject, email_template)
             
-            messages.success(request, 'Your account has been registered successfully!')
+            messages.success(request, 'Your account has been registered successfully!, Activate it by clicking the link in your mail.')
             
             return redirect('registerUser')
         else:
@@ -123,7 +123,7 @@ def registerVendor(request):
             email_template = 'accounts/emails/account_verification_email.html'
             send_verification_email(request, user, mail_subject, email_template)
 
-            messages.success(request, 'Your account has been registered successfully! Please wait for the approval.')
+            messages.success(request, 'Your account has been registered successfully! Activate it by clicking the link in your mail and Please wait for the approval.')
             return redirect('registerVendor')
 
         else:
